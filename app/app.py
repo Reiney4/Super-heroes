8e83403f815b7bfc12971c72a17839df7d525296
Valerie <valeriejeptoo4@gmail.com>
2023-10-02 11:28:44 +0300

    Updated the class home for landing  message

--- app/app.py
+++ app/app.py
@@ -2,6 +2,9 @@
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from werkzeug.exceptions import NotFound
+
 
 from models import db, Hero, Power, Hero_powers
 
@@ -10,12 +13,24 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/')
-def home():
-    return ''
+api = Api(app)
+
+# @app.route('/')
+# def home():
+#     return ''
+
+# create message for landing page 
+class Home(Resource):
+    def get(self):
+        response_message = {
+            "message": "WELCOME TO THE PIZZA SUPER HEROES API."
+        }
+        return make_response(response_message, 200)
+
+
+api.add_resource(Home, '/')
 
 
 if __name__ == '__main__':
